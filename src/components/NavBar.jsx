550cea99aa75fff5a593f9660fe5b87f3be7e81c
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-04-21 13:03:24 +0100

    navbar now maps through topics

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,35 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getTopics } from "../utils/api";
 
 export const NavBar = () => {
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    getTopics().then((topicsFromApi) => {
+      setTopics(topicsFromApi);
+    });
+  }, []);
+
   return (
     <nav className="navbar">
-      <h3>Profile</h3>
-      <select
-      name="selector"
-      id=""
-      onChange={(event) => {}}
-      >
-
-      </select>
+      <ul>
+        <h3>
+          <Link to="/" className="nav-link">
+            HOME
+          </Link>
+          {topics.map((topic) => {
+            return (
+              <li key={topic.slug}>
+                <Link to={`/topics/${topic.slug}`} className="nav-link">
+                  {topic.slug}
+                </Link>
+              </li>
+            );
+          })}
+          | My Profile
+        </h3>
+      </ul>
     </nav>
-  )
-}
+  );
+};
