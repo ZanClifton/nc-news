77e0508dd6e83f6cec32ba4ceaa13c8e5282f456
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-04-21 13:01:27 +0100

    added new route to App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/topics/:topic" element={<Home />} />
       </Routes>
     </div>
   );
