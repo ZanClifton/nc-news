d717232c7385e8d72751db017225c5718da65ead
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-04-21 15:12:14 +0100

    added get topics and params to get articles

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,8 +4,21 @@ const shrellyApi = axios.create({
   baseURL: "https://shrelly-mail-online.herokuapp.com/api",
 });
 
-export const getArticles = () => {
-  return shrellyApi.get("/articles").then(({ data }) => {
-    return data.articles;
+export const getArticles = (topic) => {
+
+  return shrellyApi
+  .get("/articles", {
+    params: {
+      topic: topic
+    }
   })
-}
+  .then(({ data }) => {
+    return data.articles;
+  });
+};
+
+export const getTopics = () => {
+  return shrellyApi.get("/topics").then(({ data }) => {
+    return data.topics;
+  });
+};
